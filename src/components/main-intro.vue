febe773869f52c9f6ee4358b661613def0f8ed96
AmitMi1 <98426263+AmitMi1@users.noreply.github.com>
2022-04-06 19:56:18 +0300

    .

--- src/components/main-intro.vue
+++ src/components/main-intro.vue
@@ -2,13 +2,10 @@
   <section class="intro">
     <div class="intro-container ml">
     <img class="img-intro" src="../assets/images/illustration-intro.png" alt="">
-
-
     <!-- <div></div> -->
     <h1>All your files in one secure location, accessible anywhere.</h1>
-    <h1>All your files in one secure location, accessible anywhere.</h1>
     <p>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
-    <button class="btn-intro">Get Start</button>
+    <button class="btn-intro">Get Started</button>
     </div>
   </section>
 </template>
