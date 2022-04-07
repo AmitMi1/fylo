e04bf828f33c2b4b8de0f4496f980ba63d4564b5
AmitMi1 <98426263+AmitMi1@users.noreply.github.com>
2022-04-07 09:04:05 +0300

    .

--- src/components/main-starter.vue
+++ src/components/main-starter.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="starter flex center">
-      <div class="starter-container ml flex center">
-          hi
+      <div class="starter-container ml flex center align-center column">
+        <h1>Get early access today</h1>
+        <p>It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you.</p>
+      <div>
+        <input type="text" placeholder="email@example.com" name="" id="">
+        <button>Get Started For Free</button>
+      </div>
       </div>
   </section>
 </template>
